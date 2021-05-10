a3f157b4474b2b6102e93a4ef5aa1cec04a6ee76
Lee X <lee52597@gmail.com>
2021-05-10 13:50:23 -0400

    Created template jsx for Collection components and flashcard components. Added axios.get for gathering all collection

--- Frontend/flashcard/src/components/App.jsx
+++ Frontend/flashcard/src/components/App.jsx
@@ -1,12 +1,24 @@
 import NavBarComponent from "./navBar/navBar";
 import React, { Component } from 'react';
+import axios from "axios";
 
 
 class App extends Component {
     state = {
-        nothing: null
+        allCollections: null,
+
     }
     
+    componentDidMount(){
+        this.gatherCollections();
+    }
+    
+    async gatherCollections(){
+        response = await axios.get(URL)
+        this.setState(
+            allCollections = response.data
+        )
+    }
 
     render() {
         return(
